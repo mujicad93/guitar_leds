0debad67eacdea577cfd2ab0f3a42bd6ba4e36a4
Andres Mujica <mujicad93@gmail.com>
2022-03-01 14:11:26 -0500

    use dim colors

--- guitar_leds.ino
+++ guitar_leds.ino
@@ -27,67 +27,67 @@ void setup() {
 #define DONT_CLEAR_OTHER_LEDS (false)
 
 void loop() {
-	Guitar_leds.set_all_notes("E", RED(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("E", RED(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_all_notes("A", RED(BRIGHT), DONT_CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("A", RED(DIM), DONT_CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_all_notes("D", RED(BRIGHT), DONT_CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("D", RED(DIM), DONT_CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_all_notes("G#", BLUE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("G#", BLUE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_all_notes("Bb", BLUE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("Bb", BLUE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_all_notes("eb", BLUE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_all_notes("eb", BLUE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_freq(196.f, GREEN(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_freq(196.f, GREEN(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_freq(220.f, GREEN(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_freq(220.f, GREEN(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_fret(0, 0, WHITE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_fret(0, 0, WHITE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_fret(1, 1, WHITE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_fret(1, 1, WHITE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_fret(2, 2, WHITE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_fret(2, 2, WHITE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_fret(3, 3, WHITE(BRIGHT), CLEAR_OTHER_LEDS);
+	Guitar_leds.set_fret(3, 3, WHITE(DIM), CLEAR_OTHER_LEDS);
 	Guitar_leds.set_leds();
 	delay(1000);
 
-	Guitar_leds.set_chord(G_MAJ_CHORD, PURPLE(BRIGHT));
+	Guitar_leds.set_chord(G_MAJ_CHORD, PURPLE(DIM));
 	Guitar_leds.set_leds();
 	delay(3000);
 
-	Guitar_leds.set_chord(A_MIN_CHORD, TEAL(BRIGHT));
+	Guitar_leds.set_chord(A_MIN_CHORD, TEAL(DIM));
 	Guitar_leds.set_leds();
 	delay(3000);
 
-	Guitar_leds.set_chord(A_MAJ_CHORD, YELLOW(BRIGHT));
+	Guitar_leds.set_chord(A_MAJ_CHORD, YELLOW(DIM));
 	Guitar_leds.set_leds();
 	delay(3000);
 
-	Guitar_leds.set_chord(D_MAJ_CHORD, ORANGE(BRIGHT));
+	Guitar_leds.set_chord(D_MAJ_CHORD, ORANGE(DIM));
 	Guitar_leds.set_leds();
 	delay(3000);
 }
