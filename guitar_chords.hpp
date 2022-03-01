d7e3ae68da5c559f87e2ee0c77934b863dddd846
Andres Mujica <mujicad93@gmail.com>
2022-03-01 13:15:29 -0500

    define some guitar chords

---
+++ guitar_chords.hpp
@@ -0,0 +1,10 @@
+#ifndef __GUITAR_CHORDS__H
+#define __GUITAR_CHORDS__H
+
+#include "guitar_leds.hpp"
+
+size_t G_MAJ_CHORD[guitar_leds::NUM_STRINGS] = { 3, 2, 0, 0, 3, 3 };
+size_t A_MAJ_CHORD[guitar_leds::NUM_STRINGS] = { guitar_leds::NUM_FRETS, guitar_leds::NUM_FRETS, 0, 2, 2, 2 };
+size_t A_MIN_CHORD[guitar_leds::NUM_STRINGS] = { guitar_leds::NUM_FRETS, guitar_leds::NUM_FRETS, 0, 2, 2, 1 };
+
+#endif
